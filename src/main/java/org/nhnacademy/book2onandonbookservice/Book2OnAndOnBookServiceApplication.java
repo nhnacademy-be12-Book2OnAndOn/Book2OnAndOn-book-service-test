0c1e354fa3c273c2bc9779df2bd00186fda42aec
Minseo Kim <porori031224@gmail.com>
2025-11-12 11:14:09 +0900

    first push

---
+++ src/main/java/org/nhnacademy/book2onandonbookservice/Book2OnAndOnBookServiceApplication.java
@@ -0,0 +1,13 @@
+package org.nhnacademy.book2onandonbookservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Book2OnAndOnBookServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Book2OnAndOnBookServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/nhnacademy/book2onandonbookservice/Book2OnAndOnBookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nhnacademy.book2onandonbookservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Book2OnAndOnBookServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
