f24eac8940006d0b87127d02d2060c1e785c6300
onyune <wjsdbwls0303@gmail.com>
2025-11-14 14:25:40 +0900

    dfa

--- src/main/java/org/nhnacademy/book2onandonbookservice/config/SecurityConfig.java
+++ src/main/java/org/nhnacademy/book2onandonbookservice/config/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/health", "/actuator/**").permitAll()  // 루트 경로 허용
+                        .requestMatchers("/**", "/health", "/actuator/**").permitAll()  // 루트 경로 허용
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults());
