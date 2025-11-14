25c0c30aed469b8c33c5a8ad7b0911cd667bf5f4
onyune <wjsdbwls0303@gmail.com>
2025-11-14 09:52:52 +0900

    Add CI/CD pipeline

---
+++ src/main/java/org/nhnacademy/book2onandonbookservice/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package org.nhnacademy.book2onandonbookservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/health", "/actuator/**").permitAll()  // 루트 경로 허용
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
---
+++ src/main/java/org/nhnacademy/book2onandonbookservice/controller/homeController.java
@@ -0,0 +1,14 @@
+package org.nhnacademy.book2onandonbookservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class homeController {
+
+    @GetMapping("/")
+    public String check(){
+        return "Server is running!";
+    }
+
+}
